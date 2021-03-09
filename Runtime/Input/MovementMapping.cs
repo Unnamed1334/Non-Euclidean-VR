8140a4e39277f04e909e72e9521474bc1c764a16
Unnamed1334 <petermathwhizz@gmail.com>
2021-03-09 09:58:03 -0500

    Added parameters to MovementMapping to support more play areas.

--- Runtime/Input/MovementMapping.cs
+++ Runtime/Input/MovementMapping.cs
@@ -15,9 +15,19 @@ public class MovementMapping : MonoBehaviour {
     public GameObject playerCamera;
 
     // Playspace configuation
+    // Maximum size of the playspace. 
     public float maxRadius = 1.0f;
+    // The maximum speed the mapping will attempt to correct.
+    // Stored as curvature as it is easier to visualize than angle per meter
+    // Too small of a value will make the camera spin wildly. Too large of a value will result in walking out of the play area.
+    public float minimumCurvature = .3f;
+    // The radius of the circle that the mappling will follow given a long period of straight movement.
+    // Too small makes movement difficult while not using most of the play area.
+    // Too large makes it easy to exit the play area with sudden dirrection changes.
+    public float targetRadius = .5f;
 
     // Feedback configuration
+
 
     private Vector3 lastCameraPosition;
 
@@ -45,39 +55,47 @@ public class MovementMapping : MonoBehaviour {
 
             // Calculating the new position in p
             float tangentRadius = Mathf.Abs(newCameraPosition.x * translation.normalized.z - newCameraPosition.z * translation.normalized.x);
-            tangentRadius = Mathf.Clamp(tangentRadius, -1, 1);
             float parallelDistance = Vector3.Dot(translation.normalized, newCameraPosition);
-            tangentRadius = Mathf.Clamp(tangentRadius, 0, 1);
+            // Do some cleanup to get good numbers. Apply max radius here as the equations are based on unit distances.
+            tangentRadius = 1 / maxRadius * tangentRadius;
+            parallelDistance = 1 / maxRadius * parallelDistance;
+            tangentRadius = Mathf.Clamp(tangentRadius, -1, 1);
+            parallelDistance = Mathf.Clamp(parallelDistance, -1, 1);
 
-            float rotationRadius = 0;
-            float idealradius = 0;
+
+            float rotationRadius;
+            float idealradius;
             // Cases:
             // -- Moveing outwards, in front of target
             if (parallelDistance > 0) {
-                if (newCameraPosition.magnitude > 1) {
-                    rotationRadius = .3f;
+                // Fallback case if the player manages to exit the play area.
+                // Use the minimum radius to try to correct.
+                if (newCameraPosition.magnitude > maxRadius) {
+                    rotationRadius = minimumCurvature;
                 }
                 else {
-                    idealradius = Mathf.Max(0.3f, 0.3f + Mathf.Pow(1 - tangentRadius, 0.0625f) * ((tangentRadius + 0.5f) / 2 - 0.3f));
-                    rotationRadius = 0.3f + (idealradius - 0.3f) * Mathf.Pow(16, -new Vector3(parallelDistance, 0, tangentRadius - .5f).magnitude);
+                    idealradius = minimumCurvature + Mathf.Pow(1 - tangentRadius, 0.0625f) * ((tangentRadius + 0.5f) / 2 - minimumCurvature);
+                    // Minimum radius;
+                    idealradius = Mathf.Max(idealradius, minimumCurvature);
+                    rotationRadius = minimumCurvature + (idealradius - minimumCurvature) * Mathf.Pow(16, -new Vector3(parallelDistance, 0, tangentRadius - .5f).magnitude);
                 }
             }
             // -- Moving inwards, behind target
             // ---- Above target
             // ---- below target
             else {
-                idealradius = Mathf.Max(0.3f, 0.3f + Mathf.Pow(1 - tangentRadius, 0.0625f) * ((tangentRadius + 0.5f) / 2 - 0.3f));
+                idealradius = Mathf.Max(minimumCurvature, minimumCurvature + Mathf.Pow(1 - tangentRadius, 0.0625f) * ((tangentRadius + 0.5f) / 2 - minimumCurvature));
                 // Strange hackery near the center to help dirrect the player away
                 if (newCameraPosition.magnitude < .2f) {
                     idealradius -= Mathf.Min(3, 3 * (2 - 10 * newCameraPosition.magnitude));
                 }
                 rotationRadius = 1 / ((1 / idealradius) * (1 + parallelDistance) - parallelDistance * (-2 + 4 * tangentRadius));
             }
-            // Convert to speed
-            float rotAmmount = RadiusToSpeed(rotationRadius);
-            // Deadzone near center
-            if (newCameraPosition.magnitude < .3f) {
-                rotAmmount *= Mathf.Max(0, 10 * newCameraPosition.magnitude - 2);
+            // Convert to speed but clamp to the bounds
+            float rotAmmount = RadiusToSpeed(Mathf.Max(maxRadius * rotationRadius, minimumCurvature));
+            // Deadzone near center to remove the central singularity
+            if (newCameraPosition.magnitude < minimumCurvature) {
+                rotAmmount *= Mathf.Clamp01(10 * newCameraPosition.magnitude - 2);
             }
 
             // Use dirrection and displacement to get the rotation dirrection
